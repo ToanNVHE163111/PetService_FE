0ec1f1e24fef7e83af6fa04723351826cbd9b846
NguyenVanThanggg02 <thang2k210@gmail.com>
2024-06-04 18:02:11 +0700

    updateScreen

--- src/DashBoard/ProductMana/Pet/EditPet.jsx
+++ src/DashBoard/ProductMana/Pet/EditPet.jsx
@@ -3,20 +3,51 @@ import axios from "axios";
 import { Button } from "primereact/button";
 import { Dialog } from "primereact/dialog";
 import { PlusSquareFill, TrashFill, X } from "react-bootstrap-icons";
-import { Col, FormSelect, Row } from "react-bootstrap";
-import '../../../style/addproduct.css'
+import { Col, Form, FormSelect, Row } from "react-bootstrap";
+import "../../../style/addproduct.css";
 
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 const EditPet = (props) => {
   const { editVisible, setEditVisible, data } = props;
+  const [breed, setBreed] = useState(data?.breed);
+  const [gender, setGender] = useState(data?.gender);
+  const [image, setImage] = useState([data?.image[0]]);
+  const [quantity, setQuantity] = useState(data?.quantity);
+  const [pettype, setPetType] = useState(data?.pettype);
+  const nav = useNavigate();
+console.log(gender);
+  const handleEditProduct = (e) => {
+    e.preventDefault();
+    axios
+      .put(`http://localhost:9999/pets/${data._id}`, {
+        breed: breed,
+        gender: gender,
+        quantity: quantity,
+        pettype: pettype,
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          toast.success("Chỉnh sửa sản phẩm thành công");
+          nav("/dashboard");
+          setEditVisible(false);
+          console.log(data);
+        } else {
+          toast.error("Chỉnh sửa sản phẩm thất bại");
+        }
+      })
+      .catch((error) => {
+        toast.error("Chỉnh sửa sản phẩm thất bại: " + error.message);
+      });
+  };
+
   const onHide = () => {
     setEditVisible(false);
   };
 
   const dialogFooter = (
-    <div style={{ margin: "20px" }}>
+    <div style={{ margin: "20px", textAlign:'end' }}>
       <Button
         className=" btn btn-success mr-2"
         type="submit"
@@ -43,31 +74,91 @@ const EditPet = (props) => {
       >
         <div className="bg-light p-1" style={{ margin: "25px" }}>
           <div style={{ margin: "40px" }}>
-            <form id="editProductForm">
+            <form id="editProductForm" onSubmit={handleEditProduct}>
               <Row>
                 <Col md={6}>
                   <div className="form-group w-full">
                     <label className="label" htmlFor="name">
-                      <h6>Product Name</h6>
+                      <h6>Breed</h6>
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="breed"
+                      value={breed}
+                      placeholder="Input breed"
+                      onChange={(e) => setBreed(e.target.value)}
+                      style={{ height: "50px" }}
+                      required
+                    />
+                  </div>
+                  <div className="form-group w-full">
+                    <Form.Group className="mb-3" controlId="formGroupGender">
+                      <label className="label" htmlFor="name">
+                        <h6>Gender</h6>
+                      </label>
+                      <Form.Select
+                        required
+                        value={gender}
+                        onChange={(e) => setGender(e.target.value)}
+                        style={{
+                          width: "100%",
+                          height: "45px",
+                          marginTop: "3px",
+                        }}
+                      >
+                        <option value="">Select Gender</option>
+
+                        <option value="Male">Male</option>
+                        <option value="Female">Female</option>
+                      </Form.Select>
+                      <p style={{ color: "red" }}></p>
+                    </Form.Group>
+                  </div>
+                  <div className="form-group w-full">
+                    <label className="label" htmlFor="name">
+                      <h6>Images</h6>
                     </label>
                     <input
                       type="text"
                       className="form-control"
                       name="name"
-                      placeholder="Input product name"
+                      value={image}
+                      onChange={(e) => setImage(e.target.value)}
+
+                      placeholder="Input images"
+                      style={{ height: "50px" }}
+                      required
+                    />
+                  </div>
+                </Col>
+                <Col md={6}>
+                  <div className="form-group w-full">
+                    <label className="label" htmlFor="name">
+                      <h6>Quantity</h6>
+                    </label>
+                    <input
+                      type="number"
+                      className="form-control"
+                      name="name"
+                      value={quantity}
+                      onChange={(e) => setQuantity(e.target.value)}
+                      placeholder="Input quantity"
                       style={{ height: "50px" }}
                       required
                     />
                   </div>
                   <div className="form-group w-full">
                     <label className="label" htmlFor="description">
-                      <h6>Description</h6>
+                      <h6>PetType</h6>
                     </label>
                     <input
                       type="text"
                       className="form-control"
                       name="description"
-                      placeholder="Input description"
+                      value={pettype}
+                      onChange={(e) => setPetType(e.target.value)}
+                      placeholder="Input pettype"
                       style={{ height: "50px" }}
                       required
                     />
