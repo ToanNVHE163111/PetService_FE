0bdfef5db1139341a19e44ab64e6321ffcac7bd7
Hoà Hữu <hoanhhe160365@fpt.edu.vn>
2024-06-04 22:16:40 +0700

    payment

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import Product_Detail from "./screens/Product_Detail";
 import Home from "./layout/Home";
 import ResetPass from "./screens/ResetPass";
 import EditProfile from "./screens/EditProfile";
+import Payment from "./screens/Payment";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -27,6 +28,8 @@ function App() {
           <Route path="/online-booking" element={<Online_Booking />} />
           <Route path="/contact" element={<ContactForm />} />
           <Route path="/editprofile" element={<EditProfile />} />
+          <Route path="/payment" element={<Payment />} />
+
           <Route
             path="/login"
             element={
